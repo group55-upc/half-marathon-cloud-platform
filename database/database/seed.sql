3ce8d2d8f1d6afce94872a1c808e336e7de5a58d
lorenteitzel <ilorente@nearbycomputing.com>
2026-06-18 16:52:26 +0200

    Add seed data

---
+++ database/database/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO races
+(name, city, country, race_date, distance, official_url, route_file)
+VALUES
+('Mitja Marató de Barcelona', 'Barcelona', 'Spain', '2027-02-15', 21.097, 'https://example.com', 's3://upc-halfmarathon-routes-dev/barcelona-2027.geojson'),
+('Mitja Marató de Valencia', 'Valencia', 'Spain', '2027-10-20', 21.097, 'https://example.com', 's3://upc-halfmarathon-routes-dev/valencia-2027.geojson'),
+('Mitja Marató de Granollers', 'Granollers', 'Spain', '2027-02-01', 21.097, 'https://example.com', 's3://upc-halfmarathon-routes-dev/granollers-2027.geojson');
