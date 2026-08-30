c6cc229f18cec70b24aaea5efdd4fdd662ecb1c7
Miquel Arjol <miquelarjol@gmail.com>
2026-08-30 22:54:44 +0300

    Automatically upload race route GeoJSON files

--- infrastructure/routes-s3.tf
+++ infrastructure/routes-s3.tf
@@ -2,9 +2,18 @@
 # RACE ROUTES S3 BUCKET
 #
 # Private bucket used to store race circuits in GeoJSON format.
+# Every .geojson file located in database/routes is uploaded automatically.
+#
 # The backend running on ECS retrieves these files and serves them to
 # the frontend through the API.
 ###############################################################################
+
+locals {
+  race_route_files = fileset(
+    "${path.module}/../database/routes",
+    "*.geojson"
+  )
+}
 
 resource "aws_s3_bucket" "race-routes" {
   bucket_prefix = "marathon-routes-"
@@ -22,12 +31,14 @@ resource "aws_s3_bucket_public_access_block" "race-routes" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_object" "barcelona-half-route" {
-  bucket = aws_s3_bucket.race-routes.id
-  key    = "routes/barcelona-half-marathon.geojson"
+resource "aws_s3_object" "race-routes" {
+  for_each = local.race_route_files
 
-  source = "${path.module}/../database/routes/barcelona-half-marathon.geojson"
-  etag   = filemd5("${path.module}/../database/routes/barcelona-half-marathon.geojson")
+  bucket = aws_s3_bucket.race-routes.id
+  key    = "routes/${each.value}"
+
+  source = "${path.module}/../database/routes/${each.value}"
+  etag   = filemd5("${path.module}/../database/routes/${each.value}")
 
   content_type = "application/geo+json"
 }
