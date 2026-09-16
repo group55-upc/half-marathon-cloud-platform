e4d2a4fc7b34ca7846db3fb0d933365c0195872a
javier <j.parce.nisa@gmail.com>
2026-09-16 18:03:47 +0000

    route 53 test

--- frontend/code/src/app/services/race.service.ts
+++ frontend/code/src/app/services/race.service.ts
@@ -17,7 +17,7 @@ export interface Race {
 })
 export class RaceService {
   private readonly http = inject(HttpClient);
-  private readonly apiUrl = 'http://alb-backend-1933529975.us-east-1.elb.amazonaws.com';
+  private readonly apiUrl = 'http://alb-backend-343043740.us-east-1.elb.amazonaws.com';
 
   getRaces(filters?: Partial<Race>): Observable<Race[]> {
     let params = new HttpParams();
